13d12ea32bb26b2a2fa67bd75ef35bf1b8552a60
leeyunjai82 <leeyunjai@circul.us>
2021-01-11 17:52:28 +0900

    update

---
+++ utils/config.py
@@ -0,0 +1,8 @@
+class Config:
+  OPENPIBO_PATH="/home/pi/openpibo"
+  OPENPIBO_DATA_PATH="/home/pi/openpibo-data"
+  TESTDATA_PATH =OPENPIBO_DATA_PATH+"/testdata"
+  PROC_PATH =OPENPIBO_DATA_PATH+"/proc"
+  MODEL_PATH=OPENPIBO_DATA_PATH+"/models"
+  GOOGLE_ACCOUNT="/home/pi/piboproject-d783ed0496cb.json"
+  KAKAO_ACCOUNT="dabfb05367a032764b5688f1602643a5"
